23997ae1e3180b8d881007c1c74b7824598bb63f
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2013-04-13 04:41:43 +0400

    Scroll bugfix

--- js/common.js
+++ js/common.js
@@ -1,28 +1,52 @@
 $(function(){
-    $('.accordion h2 > a').click(function(){
-        var ablock = $(this).closest('div').find('.accordion-content');
-        var opened = $('.accordion .opened');
+	$('.accordion h2 > a').click(function(){
+		var ablock = $(this).closest('div').find('.accordion-content');
+		var opened = $('.accordion .opened');
         
-        if(ablock.get(0) !== opened.get(0))
-        {
-            opened.removeClass('opened').slideUp();
-            ablock.addClass('opened').slideDown();
-        }
-        return false;
-    });
-    return;
-	var header = $('header').addClass('pseudofixed'),
-	win = $(window);
-	win.scroll(function(){
-        var pos = win.scrollTop();
-        if (pos < 0) {
-            pos = 0;
-        }
-        var by = $(document).height();
-        var wy = win.height();
-        if (pos > by - wy) {
-            pos = by - wy;
-        }
-        header.css('top', pos);
-	}).trigger('scroll');
+		if (ablock.get(0) !== opened.get(0)) {
+			opened.removeClass('opened').slideUp();
+			ablock.addClass('opened').slideDown();
+		}
+		return false;
+	});
+	var header = $('header'),
+	win = $(window),
+	activated = false;
+	activateScrollListener = function () {
+		if (activated) {
+			return;
+		}
+		activated = true;
+		header.addClass('pseudofixed');
+		win.scroll(function(){
+			var pos = win.scrollTop();
+			if (pos < 0) {
+			pos = 0;
+			}
+			var by = $(document).height();
+			var wy = win.height();
+			if (pos > by - wy) {
+				pos = by - wy;
+			}
+			header.css('top', pos);
+		}).trigger('scroll');
+	},
+	deactivateScrollListener = function() {
+		if (!activated) {
+			return;
+		}
+		activated = false;
+		header.removeClass('pseudofixed');
+		header.css('top', 0);
+		win.unbind('scroll');
+	},
+	onresize = function() {
+		if ($(window).width() < 1020) {
+			activateScrollListener();
+		} else {
+			deactivateScrollListener();
+		}
+	};
+	onresize();
+	$(window).resize(onresize);
 });
