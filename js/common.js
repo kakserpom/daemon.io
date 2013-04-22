4d4f38b0f5b4ee54c9c9968c37bc3bff8476d872
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2013-04-23 01:29:09 +0400

    Changes

--- js/common.js
+++ js/common.js
@@ -7,18 +7,18 @@
  **/
 (function($) {
   $.fn.hashchange = function(fn) {
-    $(window).bind("jQuery.hashchange", fn);
-    return this;
+	$(window).bind("jQuery.hashchange", fn);
+	return this;
   };
 
   $.observeHashChange = function(options) {
-    var opts = $.extend({}, $.observeHashChange.defaults, options);
-    if (isHashChangeEventSupported()) {
-      nativeVersion();
-    }
-    else {
-      setIntervalVersion(opts);
-    }
+	var opts = $.extend({}, $.observeHashChange.defaults, options);
+	if (isHashChangeEventSupported()) {
+	  nativeVersion();
+	}
+	else {
+	  setIntervalVersion(opts);
+	}
   };
 
   var locationHash = null;
@@ -26,43 +26,43 @@
   var interval = 0;
 
   $.observeHashChange.defaults = {
-    interval : 500
+	interval : 500
   };
 
   function isHashChangeEventSupported() {
-    return "onhashchange" in window;
+	return "onhashchange" in window;
   }
 
   function nativeVersion() {
-    locationHash = document.location.hash;
-    window.onhashchange = onhashchangeHandler;
+	locationHash = document.location.hash;
+	window.onhashchange = onhashchangeHandler;
   }
 
   function onhashchangeHandler(e, data) {
-    var oldHash = locationHash;
-    locationHash = document.location.hash;
-    $(window).trigger("jQuery.hashchange", {before: oldHash, after: locationHash});
+	var oldHash = locationHash;
+	locationHash = document.location.hash;
+	$(window).trigger("jQuery.hashchange", {before: oldHash, after: locationHash});
   }
 
   function setIntervalVersion(opts) {
-    if (locationHash == null) {
-      locationHash = document.location.hash;
-    }
-    if (functionStore != null) {
-      clearInterval(functionStore);
-    }
-    if (interval != opts.interval) {
-      functionStore = setInterval(checkLocationHash, opts.interval); 
-      interval = opts.interval;
-    }
+	if (locationHash == null) {
+	  locationHash = document.location.hash;
+	}
+	if (functionStore != null) {
+	  clearInterval(functionStore);
+	}
+	if (interval != opts.interval) {
+	  functionStore = setInterval(checkLocationHash, opts.interval); 
+	  interval = opts.interval;
+	}
   }
 
   function checkLocationHash() {
-    if (locationHash != document.location.hash) {
-      var oldHash = locationHash;
-      locationHash = document.location.hash;
-      $(window).trigger("jQuery.hashchange", {before: oldHash, after: locationHash});
-    }
+	if (locationHash != document.location.hash) {
+	  var oldHash = locationHash;
+	  locationHash = document.location.hash;
+	  $(window).trigger("jQuery.hashchange", {before: oldHash, after: locationHash});
+	}
   }
 
   $.observeHashChange();
