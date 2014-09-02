4b1564ec94a642e8b8ed0faad28834fed14cc53a
EzheG <edg89@mail.ru>
2014-09-02 15:36:10 +0400

    docs js

--- docs/js/main.js
+++ docs/js/main.js
@@ -132,6 +132,7 @@ function setCookie(name, value, options) {
 
 $(function(){
 	var $win = $(window),
+		$doc = $(document),
 		$body = $('body'),
 		global_lang = $('html').attr('lang');
 
@@ -144,77 +145,86 @@ $(function(){
 		$body.removeClass('force_show_sidebar');
 	});
 
-	// add anchors
-	var main_container = $('.main_container'),
-		headers = main_container.find(':header[id]');
-
-	headers.wrapInner('<div class="in anchor" />');
-
-	main_container.on('click', ':header[id] .anchor, .anchor[id] pre', function() {
-		window.location = '#' + $(this).parent().attr('id');
-	});
-
-	// обработка сцец ссылок ./ и ../
-	main_container.on('click', 'a', function(event) {
+	// ./
+	function parseLinkCurr(event) {
 		var that = $(this),
 			href = that.attr('href'),
-			postfix = '',
+			postfix = href.slice(3),
+			parent = that.parentsUntil('.main_container').last();
+
+		if(!parent.length) {
+			return false;
+		}
+
+		if( parent.is(':header') ) {
+			header = parent;
+		}
+		else {
+			curr = parent;
+
+			while(true) {
+				header = curr.prev();
+
+				if(!header.length) {
+					return false;
+				}
+
+				if(header.is(':header')) {
+					break;
+				}
+
+				curr = header;
+			}
+		}
+
+		if(header.length) {
+			return '#' + header.attr('id') + (postfix ? '/'+postfix : '');
+		}
+	}
+
+	// ../
+	function parseLinkPrev(event) {
+		var that = $(this),
+			href = that.attr('href'),
 			parent = null;
 
-		// #./link
-		if(href.indexOf('#./') === 0) {
-			event.preventDefault();
-			event.stopPropagation();
+		var matches = href.match(/^\#(\.\.\/)+/g),
+			count = (matches[0].length - 1) / 3 + 1,
+			postfix = href.slice(matches[0].length);
 
-			parent = that.parentsUntil('.main_container').last();
-			postfix = href.slice(3);
+		var curr = that,
+			header = null,
+			level = 0;
 
-			if(!parent.length) {
-				return false;
-			}
-
-			if( parent.is(':header') ) {
-				header = parent;
-			}
-			else {
-				curr = parent;
-
+		loop:
+		while(true) {
+			if(level) {
 				while(true) {
 					header = curr.prev();
 
 					if(!header.length) {
-						return false;
+						break loop;
 					}
 
-					if(header.is(':header')) {
+					if(header.is('h'+ level +'[id]')) {
 						break;
 					}
 
 					curr = header;
 				}
-			}
+			} else {
+				parent = curr.parentsUntil('.main_container').last();
 
-			if(header.length) {
-				window.location = '#' + header.attr('id') + (postfix ? '/'+postfix : '');
-			}
-		}
-		else
-		// #(../)+link
-		if(href.indexOf('#../') === 0) {
-			event.preventDefault();
-			event.stopPropagation();
+				if(!parent.length) {
+					break;
+				}
 
-			var matches = href.match(/^\#(\.\.\/)+/g);
-			var count = (matches[0].length - 1) / 3 + 1;
-			postfix = href.slice(matches[0].length);
+				if( parent.is(':header') ) {
+					header = parent;
+				}
+				else {
+					curr = parent;
 
-			var curr = that;
-			var header = null;
-			var level = 0;
-
-			loop:
-			while(true) {
-				if(level) {
 					while(true) {
 						header = curr.prev();
 
@@ -222,67 +232,56 @@ $(function(){
 							break loop;
 						}
 
-						if(header.is('h'+ level +'[id]')) {
+						if(header.is(':header')) {
 							break;
 						}
 
 						curr = header;
 					}
-				} else {
-					parent = curr.parentsUntil('.main_container').last();
-
-					if(!parent.length) {
-						break;
-					}
-
-					if( parent.is(':header') ) {
-						header = parent;
-					}
-					else {
-						curr = parent;
-
-						while(true) {
-							header = curr.prev();
-
-							if(!header.length) {
-								break loop;
-							}
-
-							if(header.is(':header')) {
-								break;
-							}
-
-							curr = header;
-						}
-					}
-
-					level = +header[0].tagName.slice(1);
-
-					if(level - count < 1) {
-						return false;
-					}
-
-					// console.log(header, level, count);
 				}
 
-				// console.log(header, level, count);
+				level = +header[0].tagName.slice(1);
 
-				if(--count < 1 || !header.length) {
-					break;
+				if(level - count < 1) {
+					return false;
 				}
-				
-				if(--level < 1) {
-					break;
-				}
-
-				curr = header;
 			}
 
-			if(header.length) {
-				window.location = '#' + header.attr('id') + (postfix ? '/'+postfix : '');
+			if(--count < 1 || !header.length) {
+				break;
 			}
+			
+			if(--level < 1) {
+				break;
+			}
+
+			curr = header;
 		}
-	});
+
+		if(header.length) {
+			return '#' + header.attr('id') + (postfix ? '/'+postfix : '');
+		}
+	}
+
+	$doc
+		.on('click', '.method code a', function(event) {
+			event.stopPropagation();
+		})
+		.on('click', ':header[id] .anchor, .anchor[id] pre', function() {
+			window.location = '#' + $(this).parent().attr('id');
+		})
+		// обработка сцец ссылок ./
+		.on('click', 'a[href^="#./"]', function(event) {
+			event.preventDefault();
+			var res = parseLinkCurr.apply(this, [event]);
+			if(res) window.location = res;
+		})
+		// обработка сцец ссылок ../
+		.on('click', 'a[href^="#../"]', function(event) {
+			event.preventDefault();
+			var res = parseLinkPrev.apply(this, [event]);
+			if(res) window.location = res;
+		});
 
 	//выбор языка
 	(function(){
@@ -305,53 +304,51 @@ $(function(){
 		});
 	})();
 
-	// remove <code> tag inside of <pre>
-	// $.each($('pre > code'), function(key, item) {
-	// 	$(item).parent().html($(item).html());
-	// });
+	// подсветка синтаксиса
+	(function(){
+		$('pre code, .code_highlight').each(function(i, block) {
+			hljs.highlightBlock(block);
+		});
 
-	$('pre code, .code_highlight').each(function(i, block) {
-		hljs.highlightBlock(block);
-	});
+		var phpTypes = {
+			'void': 1,
+			'array': 1,
+			'boolean': 1,
+			'object': 1,
+			'string': 1,
+			'integer': 1,
+			'float': 1,
+			'callable': 1,
+			'mixed': '<a href="http://php.net/manual/ru/language.pseudo-types.php" target="_blank" />'
+		};
 
-	var phpTypes = {
-		'array': 1,
-		'boolean': 1,
-		'object': 1,
-		'string': 1,
-		'integer': 1,
-		'float': 1,
-		'callable': 1,
-		'mixed': '<a href="http://php.net/manual/ru/language.pseudo-types.php" target="_blank" />'
-	};
+		// типы переменных делаем ссылками
+		$('.code_highlight, pre.inline code').each(function(){
+			$('.hljs-keyword', this).each(function(){
+				var obj = $(this);
+					text = obj.text();
 
-	// типы переменных делаем ссылками
-	$('.code_highlight, pre.inline code').each(function(){
-		$('.hljs-keyword', this).each(function(){
-			var obj = $(this);
-				text = obj.text();
+				if(phpTypes[text] === 1) {
+					obj.wrap('<a href="http://php.net/manual/'+ global_lang +'/language.types.'+ text +'.php" target="_blank" />');
+				}
+				else
+				if (phpTypes[text]) {
+					obj.wrap( phpTypes[text] );
+				}
+			});
 
-			if(phpTypes[text] === 1) {
-				obj.wrap('<a href="http://php.net/manual/'+ global_lang +'/language.types.'+ text +'.php" target="_blank" />');
-			}
-			else
-			if (phpTypes[text]) {
-				obj.wrap( phpTypes[text] );
+			var that = $(this),
+				html = that.html();
+
+			// псевдотипы: url
+			if(html.toLowerCase().indexOf('$') !== -1) {
+				html = html.replace(/(\(|,)\s(url)\s/g, "$1 <a href=\"#development/pseudotypes/$2\"><span class=\"hljs-keyword\">$2</span></a> ");
+				that.html(html);
 			}
 		});
 
-		var that = $(this),
-			html = that.html();
-
-		// псевдотипы: url
-		if(html.toLowerCase().indexOf('$') !== -1) {
-			html = html.replace(/(\(|,)\s(url)\s/g, "$1 <a href=\"#development/pseudotypes/$2\"><span class=\"hljs-keyword\">$2</span></a> ");
-			that.html(html);
-		}
-	});
-
-	$('.hljs-class .hljs-title a').addClass('hljs-title');
-
+		$('.hljs-class .hljs-title a').addClass('hljs-title');
+	})();
 
 	var currLink = null,
 		prevLink = null,
@@ -734,7 +731,7 @@ $(function(){
 			}
 		}
 
-		$(document).keydown(function(event) {
+		$doc.keydown(function(event) {
 			var superKey = event.ctrlKey || event.altKey;
 
 			// left
